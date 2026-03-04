58a33cd95b48fe953f501f516c22aef42cf9c8d4
TAN-PHUOC <hotanphuocdh2017@gmail.com>
2026-03-04 22:11:08 +0700

    update config file for fastalpr and refactor fastalpr engine modular

--- src/main.py
+++ src/main.py
@@ -8,14 +8,14 @@ from utils.draw import draw_results
 
 def main():
     detector = VehicleDetector("configs/yolo.yaml")
-    alpr = FastALPREngine()
+    alpr = FastALPREngine("configs/fastalpr.yaml")
 
     pipeline = ALPRPipeline(detector, alpr)
 
     cap = cv2.VideoCapture("assets/demo.mp4")  # webcam / video / RTSP
     width,height, fps  = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)), int(cap.get(cv2.CAP_PROP_FPS))
     vidWriter = cv2.VideoWriter("demo.mp4", cv2.VideoWriter_fourcc(*"mp4v"), fps, (width, height))
-    roi_x1, roi_y1, roi_x2, roi_y2 = 0, int(height*2/4), width, int(    height*3/4)  # Example ROI (middle horizontal band)
+    roi_x1, roi_y1, roi_x2, roi_y2 = 0, int(height*1/3), width, int(height*3/4)  # Example ROI (middle horizontal band)
     while True:
         ret, frame = cap.read()
         if not ret:
@@ -26,6 +26,7 @@ def main():
         results = pipeline.process(roi_frame)
         frame[roi_y1:roi_y2, roi_x1:roi_x2] = draw_results(roi_frame, results)
         vidWriter.write(frame)
+        print("x")
     cap.release()
     vidWriter.release()
 if __name__ == "__main__":
--- src/recognizers/fastalpr_engine.py
+++ src/recognizers/fastalpr_engine.py
@@ -1,21 +1,35 @@
+import yaml
 from fast_alpr import ALPR
 
 
 class FastALPREngine:
-    def __init__(self, detector="yolo-v9-t-384-license-plate-end2end", ocr="cct-xs-v1-global-model"):
-        self.alpr = ALPR(
-                detector_model=detector,
-                ocr_model=ocr,
-            )
+    def __init__(self, config_path: str):
+        self.cfg = self._load_config(config_path)["alpr"]
 
-    def recognize(self, image, threshold=0.6):
+        self.detector_model = self.cfg["detector_model"]
+        self.ocr_model = self.cfg["ocr_model"]
+        self.threshold = self.cfg.get("threshold", 0.6)
+
+        self.alpr = ALPR(
+            detector_model=self.detector_model,
+            ocr_model=self.ocr_model,
+        )
+
+    @staticmethod
+    def _load_config(path: str) -> dict:
+        with open(path, "r") as f:
+            return yaml.safe_load(f)
+
+    def recognize(self, image, threshold: float | None = None):
         results = self.alpr.predict(image)
 
         plates = []
         for r in results:
             conf = r.detection.confidence
-            if conf < threshold:
+
+            if conf < (threshold if threshold is not None else self.threshold):
                 continue
+
             plates.append({
                 "plate": r.ocr.text,
                 "confidence": conf,
